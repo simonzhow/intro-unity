ccd71ba68219ff383de64556401609d77f9c1f91
Simon Zhou <simon.zhou@ucla.edu>
2018-06-04 15:50:43 -0700

    Some additional changes

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -13,15 +13,15 @@ class Card extends Component {
           };
 
         return (
-            <div class="card-container">
-                <div class="card">
-                    <div class="content">
-                        <div class="front" style={frontStyle}>
+            <div className="card-container">
+                <div className="card">
+                    <div className="content">
+                        <div className="front" style={frontStyle}>
                             {/* <img className="card-photo" src={this.props.image} /> */}
                             {this.props.name}
                         </div>
                         <a target="_blank" href={this.props.link}>
-                            <div class="back">
+                            <div className="back">
                                 View
                             </div>
                         </a>                        
